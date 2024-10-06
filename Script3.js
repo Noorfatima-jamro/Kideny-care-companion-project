a39b66b263830d257756a38d6ff69c777c80cee9
Noor Fatima <aniyaaajam1275513@gmail.com>
2024-10-06 15:00:02 +0500

    Add files via upload

---
+++ Script3.js
@@ -0,0 +1,8 @@
+// Select elements
+const dailyGoalInput = document.getElementById('daily-goal');
+const setGoalBtn = document.getElementById('set-goal-btn');
+const waterAmountInput = document.getElementById('water-amount');
+const logWaterBtn = document.getElementById('log-water-btn');
+const progressBar = document.getElementById('progress-bar');
+const progressText = document.getElementById('progress-text');
+const motivationMessage = document.get
